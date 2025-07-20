c3a55ae1a23fb12496f5c3e1ce880135332562e0
amarmurmu001 <iron57441@gmail.com>
2025-07-20 12:42:55 +0530

    ping pong ws assignment fe

---
+++ src/App.tsx
@@ -0,0 +1,55 @@
+
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function App() {
+  const [socket,setSocket]=useState();
+  const [message, setMessage] = useState("");
+
+  function sendMessage() {
+
+    if(!socket){
+      return;
+    }
+    //@ts-ignore
+    socket.send(message);
+
+    setMessage("");
+  }
+
+  useEffect(() => {
+    const socket = new WebSocket("ws://localhost:8080");
+    if(!socket){
+      return;
+    }
+    setSocket(socket);
+    socket.onopen = () => {
+      console.log("WebSocket connection established");
+    };
+
+    socket.onmessage = (event) => {
+      alert("Message from server: " + event.data);
+
+    }
+    socket.onerror = (error) => {
+      console.error("WebSocket error:", error);
+    };
+
+    socket.onclose = () => {
+      console.log("WebSocket connection closed");
+    };
+  }, []);
+
+
+  return (
+    <>
+      <div style={{ display: "flex", alignItems: "center", justifyContent: "center", height: "100vh", width: "100vw", flexDirection: "column" }}>
+        <input style={{ padding: "10px" }} value={message} onChange={(e)=>{setMessage(e.target.value)}} type='text' placeholder='Message...'></input>
+        <button onClick={sendMessage}>Send</button>
+
+      </div>
+    </>
+  )
+}
+
+export default App
